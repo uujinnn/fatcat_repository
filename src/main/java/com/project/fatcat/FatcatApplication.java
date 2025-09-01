5307a24ab7fcc51aaa5eec1b43895d102fbb4d28
uujinnn <kimuujin6422@gmail.com>
2025-09-01 15:31:53 +0900

    init: new fatcat project

---
+++ src/main/java/com/project/fatcat/FatcatApplication.java
@@ -0,0 +1,13 @@
+package com.project.fatcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FatcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FatcatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/fatcat/FatcatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.fatcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FatcatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
